3c035c3d43d9033828c5c7e9a03ea619c08651b2
Aachman Mittal <m.aachman@gmail.com>
2021-12-16 16:23:41 +0530

    Setup Node and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        index: "./src/index.js",
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    mode: "development",
+};
